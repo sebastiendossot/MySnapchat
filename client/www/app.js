ec050ef498d9ffd687020af8a37aeb02d319d0ed
Izcalli <nicolasvargas33@gmail.com>
2015-02-17 20:51:14 +0100

    Gestion de la détection du support mobile ou ordinateur. Chargement de cordova en conséquence effectif

--- client/www/app.js
+++ client/www/app.js
@@ -1,17 +1,26 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-document.addEventListener('deviceready', function onDeviceReady() {
-	angular.bootstrap(document, ['myApp']);
-}, false);
+var onDeviceReady = function() {
+	angular.module('myApp', [
+		'ngRoute',
+		'myApp.view1',
+		'myApp.view2',
+		'myApp.version'
+		])
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.otherwise({redirectTo: '/view1'});
+	}]);
+	// Manually launch bootstrap
+	angular.element(document).ready(function() {
+		angular.bootstrap(document, ['myApp']);
+	});
+}
 
-angular.module('myApp', [
-	'ngRoute',
-	'myApp.view1',
-	'myApp.view2',
-	'myApp.version'
-	]).
-config(['$routeProvider', function($routeProvider) {
-	$routeProvider.otherwise({redirectTo: '/view1'});
-}]);
 
+if ( app ) {
+    // Cordova application
+    document.addEventListener('deviceready', onDeviceReady(), false);
+} else {
+    // Web page
+    onDeviceReady();
+}  
