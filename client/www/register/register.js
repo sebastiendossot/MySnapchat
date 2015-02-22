557413072fbdaf95aebcdfdc569c3a331d5b2159
boubacar2 <boubacar.fane2@gmail.com>
2015-02-22 17:23:01 +0100

    Tache 4 & 5

--- client/www/register/register.js
+++ client/www/register/register.js
@@ -8,6 +8,25 @@ angular.module('myApp.register', ['ngRoute'])
     controller: 'Register2Ctrl'
   });
 }])
+.directive('equalsTo', [function () {    
+    return {
+        restrict: 'A', 
+        scope: true,
+        require: 'ngModel',
+        link: function (scope, elem, attrs, control) {
+            var check = function () {               
+                var v1 = scope.$eval(attrs.ngModel); // attrs.ngModel = “pwd1”
+                //champ à comparer
+                var v2 = scope.$eval(attrs.equalsTo).$viewValue; // attrs.equalsTo = “pwd”
+                return v1 == v2;
+            };
+            scope.$watch(check, function (isValid) {
+                // Défini si le champ est valide
+                control.$setValidity("equalsTo", isValid);
+            });
+        }
+    };
+}])
 .controller('Register2Ctrl',['$scope', '$http', function($scope,$http) {
 	$scope.registerUser = function() {
         $http.post('/api/utilisateur', jdata)
