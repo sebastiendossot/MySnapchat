3b7471b44dac022d580d3f9eb213b55703eadba9
bchalaux <bchalaux33@gmail.com>
2015-02-24 17:41:10 +0100

    menu de navigatin mis à jour pour afficher des liens differents suivant si connecter ou non, modification de la factory api en service

--- client/www/viewNavBar/navBar.js
+++ client/www/viewNavBar/navBar.js
@@ -5,15 +5,17 @@ angular.module('myApp.viewNavBar', [])
 .controller('navBarCtrl', ['$scope', '$http', 'User', 
 	function($scope, $http, User) {
 
-		$scope.connected = User.connected
-		$scope.name = User.name
+		$scope.user = User
 
 		$scope.logout = function() {
+		    User.logout()
+		    window.location.assign('#/connection')
+		}
+
+/*function() {
 			console.log("Logout button just pressed")
 			var success = function() {
 				User.logout();
-				$scope.connected = User.connected
-				$scope.name = User.name
 				$location.assign('#/connection')
 			}
 			var error = function() {
@@ -22,16 +24,18 @@ angular.module('myApp.viewNavBar', [])
 			}
 
 			userWebService.logout({}, success, error);
-		}
+		}*/
+		 // logout n'a pas de requete sur la bd ?
 		
 		// Permet d'afficher une notification en cas de nouvelle demande d'amis
-		User.id = "507f191e810c19729de860ea"; // A supprimer
+		
+	    /*User.id = "507f191e810c19729de860ea"; // A supprimer
 		$http.get('/api/requests/'+User.id)
 		.success(function(data) {
 		   $scope.notifications = data.length;
 		})
 		.error(function(data) {
 			console.log("erreur lors de la récupération du nombre de demande d'amis");
-		})
+		})*/
 		
 	}])
